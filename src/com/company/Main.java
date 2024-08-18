3d415d4b6908853f9df383278b38bc991f862cbe
krunal21041993 <kathikar.krunal3@gmail.com>
2024-08-18 17:09:31 +0530

    Division implementation added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        System.out.println(calculator.getResult(new Addition(1,2)));
     }
 }
